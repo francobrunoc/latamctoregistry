889cf3e16914000e4b0b3e69b5e743a0bcd53983
Franco Bruno Corteletti <francobrunoc@gmail.com>
2020-10-07 18:03:59 -0300

    Add files via upload

---
+++ _nuxt/pages/redcap.18e88d4.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{803:function(o,n,e){"use strict";e.r(n);var r={name:"redcap",created:function(){window.open("http://redcap.cardiologia.org.br/"),this.$router.back()}},t=e(63),component=Object(t.a)(r,void 0,void 0,!1,null,null,null);n.default=component.exports}}]);
